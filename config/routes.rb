0a8371eb4b9dd59eb5bf81774eac4c7f991c39a3
Travis Crampton & Jessica Joly <student@epicodus.com>
2015-10-19 13:59:42 -0700

    really nice navbar that links to login and sign-up screens

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root :to => 'posts#index'
+  get "/log-in" => "sessions#new"
+  post "/log-in" => "sessions#create"
+  get "/log-out" => "sessions#destroy", as: :log_out
+  root :to => 'users#index'
 
   resources :users
 
