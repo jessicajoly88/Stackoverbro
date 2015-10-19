15cf85149adfb9cb7df8ac835f6edfbb568e5a56
Travis Crampton & Jessica Joly <student@epicodus.com>
2015-10-19 14:52:08 -0700

    Add current_user method to application controller and add to view on index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'users#index'
+
   get "/log-in" => "sessions#new"
   post "/log-in" => "sessions#create"
   get "/log-out" => "sessions#destroy", as: :log_out
-  root :to => 'users#index'
+
 
   resources :users
 
