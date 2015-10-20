6a10b087343b418c9a294b6a25b17ffa94f4f1aa
Jessica Joly & Travis Crampton <student@epicodus.com>
2015-10-20 16:31:10 -0700

    now deletes one at a time for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -20,6 +20,49 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @answer = @post.answers.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @answer = @post.answers.find(params[:id])
+    if current_user
+      if @answer.user_id == current_user.id
+        if @answer.update(answer_params)
+          redirect_to post_path(@answer.post)
+        else
+          render :edit
+        end
+      else
+        flash[:alert] = "You cannot edit an answer if you didn't write it"
+        redirect_to post_path
+      end
+    else
+      flash[:alert] = "You must be signed in to edit a post"
+      redirect_to post_path
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @answer = @post.answers.find(params[:id])
+
+      if current_user
+        if @answer.user_id == current_user.id
+          @answer.destroy
+          redirect_to post_path(@post)
+        else
+          flash[:alert] = "You cannot delete an answer if you didn't write it"
+          redirect_to post_path(@post)
+        end
+      else
+        flash[:alert] = "You must be signed in to delete an answer"
+        redirect_to posts_path
+      end
+  end
+
 
   private
 
