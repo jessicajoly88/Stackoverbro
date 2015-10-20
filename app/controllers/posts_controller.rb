e6f40a3e218354d460b1b36f4d28da431ab45de6
Jessica Joly & Travis Crampton <student@epicodus.com>
2015-10-20 10:41:47 -0700

    link to new post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,31 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = @user.posts.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to
+
+
+  end
+
 
   def show
     @post = Post.find(params[:id])
   #   # @answers = @post.answers
   end
 
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   post "/log-in" => "sessions#create"
   get "/log-out" => "sessions#destroy", as: :log_out
 
+  get "/posts" => "posts#show"
 
   resources :users do
     resources :posts
   end
+
   resources :users do
     resources :answers
   end
