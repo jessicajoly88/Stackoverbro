7825a4f2535a06f4ae7348a724454ce54eeea799
Jessica Joly & Travis Crampton <student@epicodus.com>
2015-10-20 09:33:41 -0700

    Integration spec for user log out passes.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -24,4 +24,19 @@ describe "the user process" do
     click_on "Submit"
     expect(page).to have_content 't@t.com'
   end
+
+  it "logs out a user" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in 'Email', :with => "t@t.com"
+    fill_in 'Password', :with => "peppers"
+    fill_in 'Password confirmation', :with => "peppers"
+    click_on "Submit"
+    click_link "Log in"
+    fill_in 'Email', :with => "t@t.com"
+    fill_in 'Password', :with => "peppers"
+    click_on "Submit"
+    click_link "Log Out"
+    expect(page).to have_content "You've been logged out successfully."
+  end
 end
