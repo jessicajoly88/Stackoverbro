f6830d64c9227ee9863f02e98f0c7bba30f2749f
Jessica Joly & Travis Crampton <student@epicodus.com>
2015-10-20 12:08:39 -0700

    Add functionality to add an answer to a post when signed in.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,1 +1,30 @@
 class AnswersController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @answer = @post.answers.new
+  end
+
+  def create
+    if current_user
+      @post = Post.find(params[:post_id])
+      @answer = @post.answers.new(answer_params)
+      if @answer.save
+        redirect_to post_path(@post)
+      else
+        render :new
+      end
+    else
+      flash[:alert] = "You must be logged in to reply to a post!"
+      redirect_to posts_path
+    end
+  end
+
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:content)
+  end
+
+
+end
