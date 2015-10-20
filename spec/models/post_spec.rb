4a2eda05d82eb095e2c31088d9c6bb8d42015a05
Jessica Joly & Travis Crampton <student@epicodus.com>
2015-10-20 11:44:19 -0700

    set up specs for relationships between answers posts and users

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,5 @@ describe Post do
   it {should validate_presence_of :title}
   it {should validate_presence_of :body}
   it {should belong_to :user}
+  it {should have_many :answers}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe User do
   it {should validate_confirmation_of :password}
   it {should have_many :posts}
+  it {should have_many :answers}
 end
